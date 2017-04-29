e0735b5c19a77ffdb6e48150d396a6ac8757ef42
eric920405 <eric21944998@gmail.com>
2017-04-29 15:38:16 +0800

    draw enemy

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
+var eyImg = document.createElement("img");
+eyImg.src = "images/rukia.gif"
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
 function draw(){
 	ctx.drawImage(bgImg,0,0);
+	ctx.drawImage(eyImg,96,448);
 }
 setTimeout(draw,1000);
