e795d5abb9d5a4aec462032a42ff11b0aaeca408
eric920405 <eric21944998@gmail.com>
2017-05-06 13:49:07 +0800

    add tower button

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
 var eyImg = document.createElement("img");
-eyImg.src = "images/rukia.gif"
+eyImg.src = "images/rukia.gif";
+var tbImg = document.createElement("img");
+tbImg.src = "images/tower-btn.png";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
 function draw(){
 	ctx.drawImage(bgImg,0,0);
 	ctx.drawImage(eyImg,96,448);
+	ctx.drawImage(tbImg,576,416,64,64);
 }
 setTimeout(draw,1000);
